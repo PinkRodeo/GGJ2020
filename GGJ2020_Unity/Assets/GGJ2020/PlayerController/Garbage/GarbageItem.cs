a6e3bea5e89d2c8aa502a35aa71612397d4bdb69
Damian <>
2020-02-01 23:57:43 +0100

    Added group Cleaning

---
+++ GGJ2020_Unity/Assets/GGJ2020/PlayerController/Garbage/GarbageItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class GarbageItem : MonoBehaviour
+{
+    SphereCollider collider;
+    GarbageGroup group;
+
+
+    public GarbageGroup GetGroup() { return group; }
+    void Awake()
+    {
+        collider = GetComponent<SphereCollider>();
+        collider.isTrigger = true;
+
+    }
+
+    void Start()
+    {
+        group = transform.GetComponentInParent<GarbageGroup>();
+        if (group == null)
+        {
+            Debug.LogError("Failed to find GarbageGroup for item", gameObject);
+        }
+        group.Register(this);
+    }
+
+    void Clean()
+    {
+        group.ItemCleaned(this);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        var player = collider.GetComponent<PlayerTankController>();
+        if (player != null)
+        {
+            print("cleaning");
+            Clean();
+        }
+    }
+}
