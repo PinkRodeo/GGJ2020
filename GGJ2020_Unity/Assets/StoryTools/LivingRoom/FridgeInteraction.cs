3bea6c2face2dd61f7e39070f8d9f4d9c2df77b9
Steff Kempink <mswf@live.nl>
2020-02-01 20:25:51 +0100

    Cleaned up project, optimized build

--- GGJ2020_Unity/Assets/StoryTools/LivingRoom/FridgeInteraction.cs
+++ GGJ2020_Unity/Assets/StoryTools/LivingRoom/FridgeInteraction.cs
@@ -3,6 +3,22 @@ public class FridgeInteraction : EventBase
 {
     public override void StartEvent()
     {
+        Text = "Event Text.";
+
+		{
+			var choiceA = NewEventChoice();
+			choiceA.Text = "Enter the other room.";
+			choiceA.AddReward<StartEventReward<TestEventB>>();
+			AddChoice(choiceA);
+		}
         
+		{
+			var choiceB = NewEventChoice();
+			choiceB.Text = "Look around the current room.";
+			choiceB.AddReward<StartEventReward<TestEventC>>();
+			AddChoice(choiceB);
+		}
+
+        AddContinueChoice();
     }
 }
