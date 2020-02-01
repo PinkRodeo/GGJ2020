33cda72da016f51efc9406f03daf67e26d70c28c
Damian <>
2020-02-01 16:55:56 +0100

    Added first audio version Added camera fade

---
+++ GGJ2020_Unity/Assets/GGJ2020/Developers/Damian/FadeTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(TestFade());
+    }
+
+    IEnumerator TestFade()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        CameraEffects.StartFadeToBlack(() => { print("start finished"); });
+        yield return new WaitForSecondsRealtime(3);
+
+        CameraEffects.StartFadeToGame(() => { print("end finished"); });
+    }
+}
---
+++ GGJ2020_Unity/Assets/GGJ2020/PlayerController/Camera/CameraEffects.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+
+public static class CameraEffects
+{
+    static readonly string CinemachineCamName = "CinemachineFadeCams";
+    static GameObject CinemachineCam
+    {
+        get
+        {
+            if (cam == null)
+            {
+                cam = GameObject.Find(CinemachineCamName);
+
+                if (cam == null)
+                    Debug.LogWarning($"Failed to find gameobject with name: {CinemachineCamName}");
+            };
+            return cam;
+
+        }
+    }
+
+    static GameObject cam;
+
+    //Only 1 cam tween at a time
+    static Tween ActiveTween;
+
+    public static void StartFadeToBlack(DG.Tweening.TweenCallback onComplete, Ease ease = Ease.InQuart, float duration = 1.5f)
+    {
+        Debug.Log(CinemachineCam);
+        var cam = CinemachineCam.GetComponent<CinemachineStoryboard>();
+
+        FinishTween();
+
+        ActiveTween = DOTween.To(() => cam.m_Alpha, x => cam.m_Alpha = x, 1f, duration).OnComplete(onComplete).SetEase(Ease.InQuart);// cam.m_Alpha.
+    }
+    public static void StartFadeToGame(DG.Tweening.TweenCallback onComplete, Ease ease = Ease.OutQuart, float duration = 1.5f)
+    {
+        FinishTween();
+
+        var cam = CinemachineCam.GetComponent<CinemachineStoryboard>();
+
+        ActiveTween = DOTween.To(() => cam.m_Alpha, x => cam.m_Alpha = x, 0f, duration).OnComplete(onComplete).SetEase(Ease.OutQuart);
+    }
+
+    //Fade to black and get half way there a callback
+    public static void StartFadeToBlackToGame(DG.Tweening.TweenCallback OnHalfway, Ease ease, float duration = 1.5f)
+    {
+        FinishTween();
+
+        var cam = CinemachineCam.GetComponent<CinemachineStoryboard>();
+
+        StartFadeToBlack(() =>
+        {
+            OnHalfway.Invoke();
+            StartFadeToGame(() => { });
+        });
+
+    }
+
+    static void FinishTween()
+    {
+        if (ActiveTween != null)
+        {
+            ActiveTween.Kill(true);
+        }
+    }
+
+    static bool IsATweenActive()
+    {
+        return ActiveTween != null;
+    }
+}
